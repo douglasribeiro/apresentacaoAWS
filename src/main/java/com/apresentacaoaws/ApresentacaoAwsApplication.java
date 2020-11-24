d2ed21cfde1a66399bd7d3f17104cdd2e922bb50
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-11-24 10:30:58 -0300

    Inicial
    commit inicial

---
+++ src/main/java/com/apresentacaoaws/ApresentacaoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.apresentacaoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApresentacaoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApresentacaoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apresentacaoaws/ApresentacaoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apresentacaoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApresentacaoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
