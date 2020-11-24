68503ca8b6053e4bedcd0b80145675980f3a67d5
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-11-24 11:16:36 -0300

    inclusao role para usuario

--- src/main/java/com/apresentacaoaws/domain/Usuario.java
+++ src/main/java/com/apresentacaoaws/domain/Usuario.java
@@ -3,6 +3,9 @@ package com.apresentacaoaws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.apresentacaoaws.domain.enuns.EstadoPedido;
+import com.apresentacaoaws.domain.enuns.Role;
+
 import lombok.*;
 
 @NoArgsConstructor
@@ -15,8 +18,9 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Pedido> pedidos = new ArrayList<Pedido>();
-	
+	private List<EstadoPedido> estados = new ArrayList<EstadoPedido>();
 	
 	
 
