8243446023d0206110d122c019ff115214d6d2f8
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-11-24 12:38:08 -0300

    mapeamentos

--- src/main/java/com/apresentacaoaws/domain/PedidoEstagio.java
+++ src/main/java/com/apresentacaoaws/domain/PedidoEstagio.java
@@ -2,6 +2,18 @@ package com.apresentacaoaws.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.apresentacaoaws.domain.enuns.EstadoPedido;
 
 import lombok.AllArgsConstructor;
@@ -13,11 +25,29 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class PedidoEstagio {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(columnDefinition = "text")
 	private String descricao;
+	
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizacao;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private EstadoPedido estado;
+	
+	@ManyToOne
+	@JoinColumn(name = "pedidor_id", nullable = false)
 	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id", nullable = false)
 	private Usuario usuario;
 }
--- src/main/java/com/apresentacaoaws/domain/Usuario.java
+++ src/main/java/com/apresentacaoaws/domain/Usuario.java
@@ -3,24 +3,52 @@ package com.apresentacaoaws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import com.apresentacaoaws.domain.enuns.EstadoPedido;
 import com.apresentacaoaws.domain.enuns.Role;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String nome;
+	
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+	
+	@Column(length = 100, nullable = false)
 	private String password;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	@OneToMany(mappedBy = "usuario")
 	private List<Pedido> pedidos = new ArrayList<Pedido>();
-	private List<EstadoPedido> estados = new ArrayList<EstadoPedido>();
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<PedidoEstagio> estagios = new ArrayList<PedidoEstagio>();
 	
 	
 
