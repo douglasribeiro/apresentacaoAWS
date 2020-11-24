57ba6f60e5bd3c067993ef10354f1e536502d3b7
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-11-24 17:50:44 -0300

    Service & Hash

---
+++ src/main/java/com/apresentacaoaws/service/UserService.java
@@ -0,0 +1,47 @@
+package com.apresentacaoaws.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.apresentacaoaws.domain.User;
+import com.apresentacaoaws.repository.UserRepository;
+import com.apresentacaoaws.service.util.HashUtil;
+
+@Service
+public class UserService {
+
+	@Autowired private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+	
+	public User update(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+	
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> listAll(){
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	public User login(String email, String password) {
+		password = HashUtil.getSecureHash(password);
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+	}
+}
