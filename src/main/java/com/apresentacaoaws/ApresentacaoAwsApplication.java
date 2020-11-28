e1a8a33fec3d3449c24de51647d76f8ce7ed19a8
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-11-28 14:57:01 -0300

    Config build aws

--- src/main/java/com/apresentacaoaws/ApresentacaoAwsApplication.java
+++ src/main/java/com/apresentacaoaws/ApresentacaoAwsApplication.java
@@ -2,10 +2,17 @@ package com.apresentacaoaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApresentacaoAwsApplication {
+public class ApresentacaoAwsApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApresentacaoAwsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApresentacaoAwsApplication.class, args);
 	}
