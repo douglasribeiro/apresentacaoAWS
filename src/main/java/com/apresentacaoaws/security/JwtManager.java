de538d42b6b0b9a4b30cad25eb96960fc5de32a2
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-12-08 19:15:24 -0300

    Security part 3

--- src/main/java/com/apresentacaoaws/security/JwtManager.java
+++ src/main/java/com/apresentacaoaws/security/JwtManager.java
@@ -1,14 +1,29 @@
 package com.apresentacaoaws.security;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
+
+import com.apresentacaoaws.constant.SecurityConstants;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtManager {
 
 	public String createToken(String email, List<String> roles) {
-		String jwt = null;
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_MONTH, SecurityConstants.JWT_EXP_DAY);
+		
+		String jwt = Jwts.builder()
+				.setSubject(email)
+				.setExpiration(calendar.getTime())
+				.claim(SecurityConstants.JWT_ROLE_KEY, roles)
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.API_KEY.getBytes())
+				.compact();
 		
 		return jwt;
 	}
